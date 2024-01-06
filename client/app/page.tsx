3b715f97c6aa3ded897218cf0237dd017caa10d1
Jessica Fealy <38367384+JeCFe@users.noreply.github.com>
2024-01-06 19:10:20 +0000

    Refactor client auth (#6)
    * Move the services folder
    
    * Refactoring of auth endpoints
    
    * Changed from a funky internal api call to a standard fetch

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,50 +1,39 @@
 "use client";
+import { getApiClient } from "@/services";
 import { useUser } from "@auth0/nextjs-auth0/client";
-import { Button } from "@jecfe/react-design-system";
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import { Button, Spinner } from "@jecfe/react-design-system";
+import { useState } from "react";
+
+const testApi = getApiClient().path("/auth").method("get").create();
 
 export default function Home() {
-  const { user, error, isLoading } = useUser();
+  const { error, isLoading } = useUser();
 
-  const [state, setState] = useState({
-    isLoading: false,
-    response: undefined,
-    error: undefined,
-  });
+  const [response, setResponse] = useState<string>();
+  const [apiError, setApiError] = useState<string>();
+  const [loading, setLoading] = useState<boolean>(false);
 
   const callApi = async () => {
-    setState((previous) => ({ ...previous, isLoading: true }));
-
-    try {
-      const response = await fetch("/api/protected");
-      const data = await response.json();
-
-      setState((previous) => ({
-        ...previous,
-        response: data,
-        error: undefined,
-      }));
-    } catch (error: any) {
-      setState((previous) => ({ ...previous, response: undefined, error }));
-    } finally {
-      setState((previous) => ({ ...previous, isLoading: false }));
-    }
+    setLoading(true);
+    await testApi({})
+      .then((response) => setResponse(response.data))
+      .catch((e) => setApiError(e));
+    setLoading(false);
   };
-
-  const { isLoading: stateLoading, response, error: stateError } = state;
-
-  useEffect(() => {
-    console.log(state);
-  }, [state]);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
   return (
     <div className="space-y-4">
       <Button onClick={(e) => callApi()}>Ping API</Button>
-      <div>API Response: {response ?? "No response to render"}</div>
-      <div>API Error: {stateError ?? "No error to render"}</div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <div>API Response: {response ?? "No response to render"}</div>
+          <div>API Error: {apiError ?? "No error to render"}</div>
+        </>
+      )}
     </div>
   );
 }
--- client/services/getApiClient.ts
+++ client/services/getApiClient.ts
@@ -1,11 +1,11 @@
 import { addAuth, addBaseUrl } from ".";
-import { paths } from "../../server-client";
+import { paths } from "../server-client";
 import { Fetcher } from "openapi-typescript-fetch";
 
-export const getApiClient = (accessToken?: string) => {
+export const getApiClient = () => {
   const client = Fetcher.for<paths>();
   client.configure({
-    use: [addBaseUrl(), addAuth(accessToken ?? "")],
+    use: [addBaseUrl(), addAuth()],
   });
   return client;
 };
