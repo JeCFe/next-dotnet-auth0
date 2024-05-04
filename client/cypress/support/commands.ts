2dc5c8f302aa3205c8a7179060927ac55e677678
Jessica Fealy <jessicaf091@outlook.com>
2024-05-04 20:01:24 +0100

    update

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -31,11 +31,11 @@ import "@testing-library/cypress/add-commands";
 Cypress.Commands.add("login", () => {
   cy.visit("/");
   cy.findByRole("link", { name: "Login" }).click();
-  cy.origin(Cypress.env("auth_domain"), () => {
-    cy.get('input[name="username"]').type(Cypress.env("auth_username"));
-    cy.get('input[name="password"]').type(Cypress.env("auth_password"));
+  cy.origin(Cypress.env("AUTH_DOMAIN"), () => {
+    cy.get('input[name="username"]').type(Cypress.env("AUTH_USERNAME"));
+    cy.get('input[name="password"]').type(Cypress.env("AUTH_PASSWORD"));
     cy.get('button[name="action"]').click();
   });
   cy.findByRole("link", { name: "Logout" });
-  cy.findByText(Cypress.env("auth_username"));
+  cy.findByText(Cypress.env("AUTH_USERNAME"));
 });
