2a223ff6463410278207b2345432b9356960e5ef
Jessica Fealy <jessicaf091@outlook.com>
2024-05-04 21:14:44 +0100

    Added console log for sainty checks

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -31,6 +31,7 @@ import "@testing-library/cypress/add-commands";
 Cypress.Commands.add("login", () => {
   cy.visit("/");
   cy.findByRole("link", { name: "Login" }).click();
+  console.log("Cypress Domain:", Cypress.env("CYPRESS_DOMAIN"));
   console.log(
     Cypress.env("CYPRESS_USERNAME"),
     Cypress.env("CYPRESS_PASSWORD"),
