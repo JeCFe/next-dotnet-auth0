a74b2548481e0d8aedb8144855658fb9e25a202c
Jessica Fealy <jessicaf091@outlook.com>
2024-05-09 21:25:43 +0100

    Updates to support using new logos

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,11 @@
 import { UserProvider } from "@auth0/nextjs-auth0/client";
-import { Footer, Header, NextUser } from "@jecfe/react-design-system";
+import {
+  Footer,
+  Header,
+  JecfeLogo,
+  JecfeLogoBlack,
+  NextUser,
+} from "@jecfe/react-design-system";
 import "@jecfe/react-design-system/src/tailwind.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
@@ -22,10 +28,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <UserProvider>
-        <body className="font-poppins flex min-h-screen flex-col bg-white">
-          <Header title="JeCFe - Template" user={<NextUser />} />
+        <body className="flex min-h-screen flex-col bg-white font-poppins">
+          <Header
+            logo={<JecfeLogo height="48" />}
+            title="JeCFe - Template"
+            user={<NextUser />}
+          />
           <div className="container mx-auto flex-1">{children}</div>
-          <Footer>JeCFe - Template</Footer>
+          <Footer logo={<JecfeLogoBlack height="32" />}>
+            <div className="flex justify-center"> JeCFe - Template</div>
+          </Footer>
         </body>
       </UserProvider>
     </html>
